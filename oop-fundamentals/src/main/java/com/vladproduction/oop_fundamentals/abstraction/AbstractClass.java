f0baa9d9789e185f730d6a39cfd99f5cd316cfad
vladproduction <vladbogdantsev@gmail.com>
2025-06-17 17:25:40 +0300

    oop-fundamentals module init

---
+++ oop-fundamentals/src/main/java/com/vladproduction/oop_fundamentals/abstraction/AbstractClass.java
@@ -0,0 +1,11 @@
+package com.vladproduction.oop_fundamentals.abstraction;
+
+public abstract class AbstractClass {
+
+    public void abstractMethod(){
+        System.out.println("Abstract Method");
+    }
+
+    public abstract void abstractMethod2();
+
+}
